32059ec02e3eed022c3b72b16a51dbf5c45f2396
mizutoki_ <a.p9xelic5@gmail.com>
2018-08-25 18:57:07 +0900

    Update prompt that is able to change responsively

---
+++ Dependon_posh-git/MyPrompt.ps1
@@ -0,0 +1,62 @@
+# this depend on posh-git
+# import posh-git module before this imported
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    
+    # # Reset color, which can be messed up by Enable-GitColors
+    # $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    $promptCurrentDateTime = "$(Get-Date -Format 'MM/dd HH:mm:ss')"
+    $promptPath = (Get-Location).Path.Trim('\').Replace($env:USERPROFILE, '~')
+    $PathLeaf = [System.Math]::Min($promptPath.Length, $(Split-Path -Path $promptPath -Leaf).Length)
+    $currentRawUI = (Get-Host).ui.RawUI
+    $promptLimitLength = [System.Math]::Min($currentRawUI.BufferSize.Width, $currentRawUI.WindowSize.Width)
+    if ($promptCurrentDateTime.Length + $PathLeaf.Length + 3 -le $promptLimitLength) {
+        $promptPathLimitLength = $promptLimitLength - $promptCurrentDateTime.Length - 1
+        if (($promptPath.Length -gt $promptPathLimitLength)) {
+            if ($promptPath.Split('\').Count -gt 3) {
+                $currentLocation = $promptPath
+                if ($promptPath.ToString().Contains('~')) {
+                    $pathQualifier = '~'
+                }
+                else {
+                    $pathQualifier = Split-Path $promptPath -Qualifier
+                }
+                $PathLeaf = (Split-Path $promptPath -Parent | Split-Path -Leaf) + '\' `
+                    + (Split-Path $promptPath -Leaf)
+                if ($PathLeaf.Length -le $promptPathLimitLength - $pathQualifier.Length - 5) {
+                    $promptPath = $pathQualifier + '\...'
+                    $substringStart = $currentLocation.Length - $promptPathLimitLength + $promptPath.Length
+                    $substringLength = $promptPathLimitLength - $promptPath.Length - $PathLeaf.Length
+                    if ($substringLength -gt 0) {
+                        $promptPath = $pathQualifier + '\...' `
+                            + $currentLocation.Substring($substringStart, $substringLength) `
+                            + $PathLeaf
+                    }
+                    else {
+                        $promptPath = $pathQualifier + '\...\' `
+                            + $PathLeaf
+                    }
+                }
+                elseif (($PathLeaf = Split-Path -Path $PathLeaf -Leaf).Length -le $promptPathLimitLength) {
+                    $promptPath = "[$PathLeaf]"
+                }
+            }
+        }
+        $prompt = Write-Prompt -Object $promptCurrentDateTime -BackgroundColor ([System.ConsoleColor]::DarkMagenta) -ForegroundColor ([System.ConsoleColor]:: White)
+        $prompt += Write-Prompt -Object ' '
+        $prompt += Write-Prompt -Object $promptPath -ForegroundColor ([System.ConsoleColor]::Green) 
+        $prompt += Write-Prompt -Object "`nPS"
+        if ($promptLimitLength -gt 30) {
+            $prompt += Write-VcsStatus
+        }
+    }
+    elseif (($promptLeaf = (Split-Path -Path $promptPath -Leaf)).Length -le $promptLimitLength - 5) {
+        $prompt = Write-Prompt -Object "[$promptLeaf]" -ForegroundColor ([System.ConsoleColor]::Green)
+        $prompt += Write-Prompt -Object "`nPS"
+    }
+    else {
+        $prompt = Write-Prompt -Object 'PS'
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    if ($prompt) {"$prompt> "} else {"> "}
+}
--- SettingForPoshGit.ps1
+++ SettingForPoshGit.ps1
@@ -1,5 +1,5 @@
 ﻿# posh-git のインポート
-Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+Push-Location -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 $moduleName = "posh-git"
 if (-not (Get-Module -Name $moduleName)) {
     # If you installed "posh-git" by `Install-Module` from PowerShell Gallery
@@ -9,7 +9,7 @@ if (-not (Get-Module -Name $moduleName)) {
     else {
         # If you installed "posh-git" by Chocolatey, maybe you can't find "posh-git" by "Get-Module -ListAvailable".
         # You can find it if you use "-All" option. But that command takes time. you should specify fullpath.
-        $env:PSModulePath = $env:PSModulePath + ";C:\tools\"
+        $env:PSModulePath = "C:\tools\;" + $env:PSModulePath
         $poshGitModule = Get-Module $moduleName -All -ListAvailable | Sort-Object -Descending -Property Version | Select-Object -Index 0
         if ($poshGitModule) {
             $poshGitModule | Import-Module
@@ -19,10 +19,12 @@ if (-not (Get-Module -Name $moduleName)) {
         }
     }
 }
-
 # If module is installed in a default location ($env:PSModulePath),
 # use this instead (see about_Modules for more information):
 # Import-Module posh-git
+if (Get-Module -Name $moduleName) {
+    Get-ChildItem -Path *$moduleName* -Recurse -Include *.ps1 | ForEach-Object {& $_.FullName}
+}
 
 Remove-Variable -Name moduleName
 Pop-Location
