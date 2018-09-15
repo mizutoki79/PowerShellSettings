d93c6a0ee4d123080024502db4199cb2fa56cfb4
mizutoki_ <a.p9xelic5@gmail.com>
2018-09-15 18:05:31 +0900

    Modify the directory when WSL

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,9 @@
 if ($IsWindows) {
     $autoloadDir = Join-Path -Path ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::MyDocuments)) -ChildPath '\PowerShell\AutoloadProfile'
 }
+# is WSL. `ln -s /mnt/c/Users/(username)/Documents ~/Documents`
 elseif ($IsLinux -and (uname --kernel-release).Contains('Microsoft')) {
-    $autoloadDir = Join-Path -Path $env:PWD -ChildPath '/Documents/PowerShell/AutoloadProfile'
+    $autoloadDir = Join-Path -Path (readlink -f ~/Documents) -ChildPath '/PowerShell/AutoloadProfile'
 }
 else {
     $autoloadDir = Join-Path -Path (Split-Path -Path $PROFILE -Parent) -ChildPath '/AutoloadProfile'
