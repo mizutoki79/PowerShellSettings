9140ec01ba5c6fc1ca41d4ef2f19aeb1e263f725
mizutoki_ <a.p9xelic5@gmail.com>
2018-11-04 00:40:47 +0900

    Update alias for kaoriya's vim

--- AutoloadProfile/MyAliases.ps1
+++ AutoloadProfile/MyAliases.ps1
@@ -5,3 +5,10 @@ Set-Alias -Name cro -Value Compare-Object
 Set-Alias -Name sco -Value Select-Object
 Set-Alias -Name jpa -Value Join-Path
 Get-Verb | ForEach-Object {New-Alias -Name ($_.Verb + '-') -Value Out-Null -Description 'TAB補完用ダミーalias' -Option ReadOnly -ErrorAction SilentlyContinue}
+if (Test-Path -Path Env:\USERAPPLICATIONS) {
+    $binaryPath = Join-Path -Path $env:USERAPPLICATIONS -ChildPath 'vim81-kaoriya-win64' -AdditionalChildPath 'vim.exe'
+    if (Test-Path -Path $binaryPath) {
+        Set-Alias -Name vim -Value $binaryPath
+    }
+    Remove-Variable -Name binaryPath
+}
